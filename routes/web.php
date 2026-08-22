ad14c4afc08e4369277b5df5f4420acca5e9d519
prashanttanwar124 <prashanttanwar124@gmail.com>
2026-08-22 09:23:22 +0530

    Add explicit sitemap.xml and robots.txt route endpoints with proper headers

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,21 @@ Route::inertia('/contact', 'Contact')->name('contact');
 Route::inertia('/collections', 'Collections')->name('collections');
 Route::inertia('/about', 'About')->name('about');
 
+// --- SEO SITEMAP & ROBOTS ---
+Route::get('/sitemap.xml', function () {
+    $path = public_path('sitemap.xml');
+    return file_exists($path)
+        ? response(file_get_contents($path), 200, ['Content-Type' => 'application/xml'])
+        : abort(404);
+})->name('sitemap');
+
+Route::get('/robots.txt', function () {
+    $path = public_path('robots.txt');
+    return file_exists($path)
+        ? response(file_get_contents($path), 200, ['Content-Type' => 'text/plain'])
+        : abort(404);
+})->name('robots');
+
 // --- CUSTOMER DIGITAL VAULT ---
 Route::get('/vault/{token}', [CustomerVaultController::class, 'show'])->name('vault.show');
 Route::get('/vault/{token}/invoices/{invoice}/print', [CustomerVaultController::class, 'printInvoice'])->name('vault.invoice.print');
